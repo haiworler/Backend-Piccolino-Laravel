b50142c606a5655bfda437936cbafaa27cbfae92
Luis Garizabalo <luis.garizabalo@eltransporte.org>
2020-05-16 10:20:47 -0500

    [Lucho] Se ajusta el config/app.php para colocar las llaves primarias

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => 'America/Bogota',
 
     /*
     |--------------------------------------------------------------------------
@@ -119,7 +119,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY','base64:y7FAjcyFucq1sp7P54Vb2R2eWDC+tFVVTAwrrxzXTsI='),
 
     'cipher' => 'AES-256-CBC',
 
