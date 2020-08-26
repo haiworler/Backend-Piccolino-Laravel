e7858b1c4b23cd1975efe5c66526727917237971
Luis Garizabalo <luis.garizabalo@eltransporte.org>
2020-08-26 08:54:56 -0500

    [Luis] Se realizan ajustes

--- database/seeds/GenderSeeder.php
+++ database/seeds/GenderSeeder.php
@@ -14,5 +14,7 @@ class GenderSeeder extends Seeder
     {
         Gender::create(['name' => 'Masculino']);
         Gender::create(['name' => 'Femenino']);
+        Gender::create(['name' => 'Otros']);
+
     }
 }
